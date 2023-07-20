5d9bc1f3688e3440f7001fda8df637b5ab60edce
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-07-20 16:01:48 +0100

    Add tilt for main image

--- components/layout/hero/ImageParallax.tsx
+++ components/layout/hero/ImageParallax.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
+import Tilt from "react-parallax-tilt";
 
 const ImageParallax = () => {
   let { scrollYProgress } = useScroll();
@@ -9,19 +10,21 @@ const ImageParallax = () => {
 
   return (
     <>
-      <motion.div
-        style={{
-          y: y,
-        }}
-      >
-        <Image
-          className="relative z-30 w-[20rem] drop-shadow-2xl sm:w-[20rem] md:w-[25rem] lg:w-[35rem]"
-          src="/images/fox.webp"
-          alt="Picture of me as a fox"
-          width={500}
-          height={450}
-        />
-      </motion.div>
+      <Tilt tiltMaxAngleX={15} tiltMaxAngleY={15} gyroscope={true}>
+        <motion.div
+          style={{
+            y: y,
+          }}
+        >
+          <Image
+            className="relative z-30 w-[20rem] drop-shadow-2xl sm:w-[20rem] md:w-[25rem] lg:w-[35rem]"
+            src="/images/fox.webp"
+            alt="Picture of me as a fox"
+            width={500}
+            height={450}
+          />
+        </motion.div>
+      </Tilt>
     </>
   );
 };
